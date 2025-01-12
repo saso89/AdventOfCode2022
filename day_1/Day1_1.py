9e5e1c5153b8f74f0734f66c8bb475e221e6aade
bobnar <93672119+saso89@users.noreply.github.com>
2025-01-12 21:44:07 +0100

    Add files via upload

---
+++ day_1/Day1_1.py
@@ -0,0 +1,12 @@
+f = open("day_1.txt", encoding="utf8")
+content = f.read().split('\n\n')
+listOfSums =  []
+
+for elf in content:
+    totalSum = 0
+    for num in elf.split('\n'):
+        totalSum = totalSum + int(num)
+    listOfSums.append(totalSum)
+
+listOfSums.sort()
+print(listOfSums[-1])
