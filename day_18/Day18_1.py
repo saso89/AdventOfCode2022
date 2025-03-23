1c3b83fcbc9f76443ec8c2b3eb283889f4b3b0ea
bobnar <93672119+saso89@users.noreply.github.com>
2025-03-23 18:46:06 +0100

    Add files via upload

--- day_18/Day18_1.py
+++ day_18/Day18_1.py
@@ -3,29 +3,29 @@
 import numpy as np
 
 #kocko napolnemo s pikami
-content = [line.replace("\n","") for line in open("day_18.txt", encoding="utf8").readlines()]
-cube = np.full((25,25,25), '.', dtype=str)
+podatki = [line.replace("\n","") for line in open("day_18.txt", encoding="utf8").readlines()]
+kocka = np.full((25,25,25), '.', dtype=str)
 
-#nastavimo dobljene koordinate iz inputa kot #
-for line in content:
+#dobljene koordinate iz podatkov nastavimo kot #
+for line in podatki:
     x, y, z = map(int, line.split(','))
-    cube[x,y,z] = '#'
+    kocka[x,y,z] = '#'
 
-exposed_sides = 0
+vidne_strani = 0
 
-#zgolj iteriramo po vseh elementih najdemo sosede in preverimo kateri so pika
+#iteriramo po vseh elementih, najdemo sosede in preverimo, kateri so pika
 for x in range(25):
     for y in range(25):
         for z in range(25):
-            if cube[x, y, z] == '#':
-                neighbors = [
-                    cube[x - 1, y, z],
-                    cube[x + 1, y, z],
-                    cube[x, y - 1, z],
-                    cube[x, y + 1, z],
-                    cube[x, y, z - 1],
-                    cube[x, y, z + 1]
+            if kocka[x, y, z] == '#':
+                sosedje = [
+                    kocka[x - 1, y, z],
+                    kocka[x + 1, y, z],
+                    kocka[x, y - 1, z],
+                    kocka[x, y + 1, z],
+                    kocka[x, y, z - 1],
+                    kocka[x, y, z + 1]
                 ]
-                exposed_sides += neighbors.count('.')
+                vidne_strani += sosedje.count('.')
 
-print(exposed_sides)
+print(vidne_strani)
--- day_18/Day18_2.py
+++ day_18/Day18_2.py
@@ -3,43 +3,43 @@
 import numpy as np
 from collections import deque
 
-#bfs algoritem - preverimo če pridemo do zunanjega roba
-def seenFromOutside(x,y,z,c):
+# Breadth First Search - iskanje v širino
+def vidnoOdZunaj(x,y,z,c):
     queue = deque()
     queue.append((x, y, z))
-    visited = set()
+    obiskano = set()
 
     while queue:
         x, y, z = queue.popleft()
-        if (x, y, z) in visited:
+        if (x, y, z) in obiskano:
             continue
-        visited.add((x, y, z))
+        obiskano.add((x, y, z))
         if x < 0 or x > 24 or y < 0 or y > 24 or z < 0 or z > 24:
             return True
         if c[x,y,z] == '#':
             continue
-        for dx, dy, dz in neigbourCoordinates:
+        for dx, dy, dz in koordinateSoseda:
             queue.append((x+dx, y+dy, z+dz))
     return False
 
 #kocko napolnemo s pikami
-content = [line.replace("\n", "") for line in open("day_18.txt", encoding="utf8").readlines()]
-cube = np.full((25, 25, 25), '.', dtype=str)
-neigbourCoordinates = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
+podatki = [line.replace("\n", "") for line in open("day_18.txt", encoding="utf8").readlines()]
+kocka = np.full((25, 25, 25), '.', dtype=str)
+koordinateSoseda = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
 
-#nastavimo dobljene koordinate iz inputa kot #
-for line in content:
+#dobljene koordinate iz podatkov nastavimo kot #
+for line in podatki:
     x, y, z = map(int, line.split(','))
-    cube[x,y,z] = '#'
+    kocka[x,y,z] = '#'
 
-exposed_sides = 0
+vidne_strani = 0
 
 for x in range(25):
     for y in range(25):
         for z in range(25):
-            if cube[x, y, z] == '#':
-                for dx,dy,dz in neigbourCoordinates:
+            if kocka[x, y, z] == '#':
+                for dx,dy,dz in koordinateSoseda:
                     #preverimo če pridemo do zunanjega roba
-                    if seenFromOutside(x+dx,y+dy,z+dz,cube): exposed_sides += 1
+                    if vidnoOdZunaj(x+dx,y+dy,z+dz,kocka): vidne_strani += 1
 
-print(exposed_sides)
+print(vidne_strani)
