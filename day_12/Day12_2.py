6970cc596291c4883320d7e195a2967e2490b41f
bobnar <93672119+saso89@users.noreply.github.com>
2025-03-23 16:12:35 +0100

    Add files via upload

--- day_12/Day12_2.py
+++ day_12/Day12_2.py
@@ -1,24 +1,25 @@
 #Day 12:
 #Part 2:
 from collections import deque
-content = open("day_12.txt", encoding="utf8").readlines()
-grid = []
+podatki = open("day_12.txt", encoding="utf8").readlines()
+mreza = []
 
 #preberemo in shranimo v seznam (matrika)
-for line in content:
-    grid.append(list(line.replace('\n', '')))
+for line in podatki:
+    mreza.append(list(line.replace('\n', '')))
 
-#začetno pozicijo nastavimo kot a in končno pozicijo kot z
-for i, row in enumerate(grid):
+# začetni položaj je a - najnižja višina, lahko tudi položaj S
+# končni položaj z najboljšim signalom (E) je z - najvišja višina
+for i, row in enumerate(mreza):
     for j, item in enumerate(row):
         if item == "S":
             sx = i
             sy = j
-            grid[i][j] = "a"
+            mreza[i][j] = "a"
         if item == "E":
             ex = i
             ey = j
-            grid[i][j] = "z"
+            mreza[i][j] = "z"
 
 #definiramo vrsto in vanjo dodamo začetno koordinato
 q = deque()
@@ -30,13 +31,13 @@ visited = {(ex, ey)}
 while q:
     distance, x1, y1 = q.popleft()
     for x2, y2 in [(x1 + 1, y1), (x1 - 1, y1), (x1, y1 + 1), (x1, y1 - 1)]:
-        if x2 < 0 or y2 < 0 or x2 >= len(grid) or y2 >= len(grid[0]):
+        if x2 < 0 or y2 < 0 or x2 >= len(mreza) or y2 >= len(mreza[0]):
             continue
         if (x2, y2) in visited:
             continue
-        if ord(grid[x2][y2]) - ord(grid[x1][y1]) < -1:
+        if ord(mreza[x2][y2]) - ord(mreza[x1][y1]) < -1:
             continue
-        if grid[x2][y2] == "a":
+        if mreza[x2][y2] == "a":
             print(distance + 1)
             exit(0)
         visited.add((x2, y2))
